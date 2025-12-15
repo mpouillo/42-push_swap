080f6a481e4710d7afbdd5ab0635f175a10ab739
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2025-12-15 15:00:09 +0100

    test: updated test_main.c

--- tests/test_main.c
+++ tests/test_main.c
@@ -11,7 +11,6 @@ int main(int argc, char **argv)
 		ft_printf("Creating empty stack (no arguments passed to the function)\n");
 		stack_a = stack_create(NULL, 0);
 	}
-	rra(stack_a);
 
 	test_disorder(stack_a);
 	test_stack(stack_a);
