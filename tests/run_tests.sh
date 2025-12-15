f8b7d2e8d7bdef1ef41a9f22e97c996c86289fb4
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2025-12-15 14:55:47 +0100

    test: small fix to run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,5 +4,5 @@ SRC_FILES=$(find srcs/ -name "*.c" ! -name "push_swap.c")
 
 make &&
 cc -Wall -Werror -Wextra -g -o test.out $SRC_FILES tests/*.c -Llibftprintf -lftprintf \
-&& valgrind -s --leak-check=full --show-leak-kinds=all ./test.out $@
+&& valgrind -s --leak-check=full --show-leak-kinds=all ./test.out $@ \
 && rm ./test.out
