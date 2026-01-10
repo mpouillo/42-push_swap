119ffd1ad59693fbf18cf0fde475cb0a85807f1e
Marceau Pouilloux <mpouillo@student.42lyon.fr>
2026-01-10 14:12:32 +0100

    test: removed unnecessary call to stack_delete()

--- tests/test_disorder.c
+++ tests/test_disorder.c
@@ -7,5 +7,4 @@ void	test_disorder(t_pushswap *data)
 	stack_init_a(data);
 	disorder = compute_disorder(*data->a);
 	ft_printf("Disorder = %i.%.2i\n", disorder / 1000, disorder % 1000 / 10);
-	stack_delete(data->a);
 }
